f9de9f662567ef29c5942287828fd28fb0746df5
Awuzaso <awuzaso@gmail.com>
2016-06-09 17:23:01 -0500

    Added some documentation.Again.And again.

--- working_set_osa/workingSetProject/Open_WS_Window.swift
+++ working_set_osa/workingSetProject/Open_WS_Window.swift
@@ -9,26 +9,51 @@
 import Cocoa
 
 class Open_WS_Window: NSViewController {
-
+//////////////////////////////////////////////////////////////////////////////////////////
+/* Note: START OF CLASS                                                                  */
+//////////////////////////////////////////////////////////////////////////////////////////
+    
+    
+    var smartFolder:String?
+    
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*  FUNCTION:      'override func viewDidLoad()'                                   */
+    //////////////////////////////////////////////////////////////////////////////////////////
+    /*Note: Function, 'viewDidLoad()' is called when the view UI of this class is loaded on the display,
+     indicating that it it ready to perform some action. Can be thought of as the "main" of this View
+     Controller. www.reddit.com/r/swift/comments/39v3s6/what_exactly_is_the_purpose_of_viewdidload/ */
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Open_WS_Window is opened.")
         // Do view setup here.
     }
     
     
+    
     @IBAction func dismissOpenWindow(sender: NSButton) {
         
-       let smartFolder = "/Users/Osa/Desktop"
+       smartFolder = "/Users/Osa/Desktop"
         
-        NSWorkspace.sharedWorkspace().selectFile(nil, inFileViewerRootedAtPath: smartFolder)
+        if(smartFolder != nil){
+        NSWorkspace.sharedWorkspace().selectFile(nil, inFileViewerRootedAtPath: smartFolder!)
         
         
         let application = NSApplication.sharedApplication()
         application.stopModal()
+        }
+        else {
+            return
+        }
     }
+    
+    
     
     @IBAction func CancelWindow(sender: NSButton) {
         let application = NSApplication.sharedApplication()
         application.stopModal()
     }
+    
+//////////////////////////////////////////////////////////////////////////////////////////
+/* Note: END OF CLASS                                                                  */
+//////////////////////////////////////////////////////////////////////////////////////////
 }
